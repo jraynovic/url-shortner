bd19e64a14282083252f0ba3bcd5432d3fdcd26a
= <jraynovic@key.net>
2022-03-06 15:34:28 -0800

    build: styled header

--- client/src/components/HeaderComponent.js
+++ client/src/components/HeaderComponent.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {Grid} from '@mui/material'
 
 const HeaderComponent = () => {
   return (
-    <div>HeaderComponent
-    </div>
+    <Grid container style={{height:'10vh', marginBottom:20, paddingTop:10}}>
+      <Grid xs={6} md={4} style={{height:'10vh'}}>
+        <a href="https://www.josephraynovic.com/"
+          target="_blank">
+          <img src="https://www.josephraynovic.com/static/media/logo.7303cc08.png" height='100%' width='auto'/>
+        </a>
+        
+      </Grid>
+      <Grid xs={0} md={4}>
+      </Grid>
+      <Grid xs={6} md={4} style={{ display:'flex', alignItems:'center', justifyContent:'end'}}> 
+         <a style={{textDecoration:'none', color:'black'}} href = "mailto: josephraynovic@gmail.com">Contact</a>  
+      </Grid>
+    
+    </Grid>
   )
 }
 
