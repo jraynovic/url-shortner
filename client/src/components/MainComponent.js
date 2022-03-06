799ef8c9482dab9d917a4e3e1eede5b8c7e17757
= <jraynovic@key.net>
2022-03-06 11:59:16 -0800

    build: component folder structure

---
+++ client/src/components/MainComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeaderComponent from './HeaderComponent'
+import BodyComponent from './BodyComponent'
+import FooterComponent from './FooterComponent'
+const MainComponent = () => {
+  return (
+    <div>
+        <HeaderComponent/>
+        <BodyComponent/>
+        <FooterComponent/>
+    </div>
+  )
+}
+
+export default MainComponent
