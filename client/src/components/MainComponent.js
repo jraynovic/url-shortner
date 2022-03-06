0a517808cded44fec3ade37f1b52c3dd9bd99e12
= <jraynovic@key.net>
2022-03-06 15:43:45 -0800

    refactor: removed footer component

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Container, Grid } from '@mui/material'
 import HeaderComponent from './HeaderComponent'
 import BodyComponent from './BodyComponent'
-import FooterComponent from './FooterComponent'
 
 const MainComponent = () => {
   return (
@@ -12,9 +11,6 @@ const MainComponent = () => {
       </Grid>
       <Grid item xs={12}>
         <BodyComponent />
-      </Grid>
-      <Grid item xs={12}>
-        <FooterComponent />
       </Grid>
     </Container>
   );
