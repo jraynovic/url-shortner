e0dc7f280d2cd351d12d41df8c4d2f7707870f95
= <jraynovic@key.net>
2022-03-06 10:10:01 -0800

    build: resovlers structure. Tested function

--- Schema/resolvers.js
+++ Schema/resolvers.js
@@ -1,5 +1,17 @@
 const resolvers = {
+  Query:{
+    async getOneLink(parent, args){
+        console.log(args)
+        return{ url:'test get url', slug:'test get slug' } 
+    }
+  },
 
+  Mutation:{
+      async createLink(parent, args){
+          console.log(args)
+          return {url:'test url', slug:'test slug'}
+      }
+  }
 }
 
 module.exports = {resolvers}
